69cea567331a25f2a7f111e91f829896e478d98b
Daniel Rodrigues <contatodanielgit@gmail.com>
2023-08-08 10:13:45 -0300

    "trocado .toggleClass por .addClass"

--- Módulo 9 - Exercício/main.js
+++ Módulo 9 - Exercício/main.js
@@ -2,16 +2,14 @@ $(document).ready(function () {
     
     $("form").on("submit", function(e) {
         e.preventDefault()
-
+        
         const nomeTarefa = $("#nome-tarefa").val()
         const novaTarefa = `<li>${nomeTarefa}</li>`
 
-        $(nomeTarefa).appendTo(novaTarefa) // Com o .appendTo, o primeiro valor é colocado dentro do segundo valor
-
-        $(novaTarefa).appendTo("ul")
+        $(novaTarefa).appendTo("ul") // Com o .appendTo, o primeiro valor é colocado dentro do segundo valor
 
         $("li").click(function () {
-            $(this).toggleClass("sucesso")
+            $(this).addClass("sucesso")
         })
 
         $("#nome-tarefa").val("") // Para atualizar o form ao clicar no botão cadastrar
