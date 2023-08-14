3ad8faf484f9bfdc883707a375f990ac58b9e9ab
Daniel Rodrigues <contatodanielgit@gmail.com>
2023-08-14 14:08:52 -0300

    commit inicial

---
+++ Módulo 10 - Exercício/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("#cpf").mask("000.000.000-00", {
+    })
+
+    $("#cep").mask("00000-000", {
+    })
+
+    $("#telefone").mask("(00) 0 0000-0000", { // Aplicando máscara ao campo telefone
+    })
+
+    })
